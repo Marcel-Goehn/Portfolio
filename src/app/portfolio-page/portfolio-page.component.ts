cf42a97953e4fb7a31f8fb6b759414050f5de8df
Marcel Göhn <marcelgoehn@googlemail.com>
2025-08-03 12:37:59 +0200

    Added headings and navigation links to the hero section

--- src/app/portfolio-page/portfolio-page.component.ts
+++ src/app/portfolio-page/portfolio-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HeroComponent } from "./hero/hero.component";
 
 @Component({
   selector: 'app-portfolio-page',
   standalone: true,
-  imports: [],
+  imports: [HeroComponent],
   templateUrl: './portfolio-page.component.html',
   styleUrl: './portfolio-page.component.scss'
 })
