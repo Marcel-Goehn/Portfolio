5fde1a5907de733c3b973d77291d84ee53171ba6
Marcel Göhn <marcelgoehn@googlemail.com>
2025-08-06 21:05:37 +0200

    Added about me section, Added icons for the section

--- src/app/portfolio-page/portfolio-page.component.ts
+++ src/app/portfolio-page/portfolio-page.component.ts
@@ -1,10 +1,11 @@
 import { Component, HostListener } from '@angular/core';
 import { HeroComponent } from "./hero/hero.component";
+import { AboutMeComponent } from "./about-me/about-me.component";
 
 @Component({
   selector: 'app-portfolio-page',
   standalone: true,
-  imports: [HeroComponent],
+  imports: [HeroComponent, AboutMeComponent],
   templateUrl: './portfolio-page.component.html',
   styleUrl: './portfolio-page.component.scss'
 })
