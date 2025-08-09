4366d8435e6e935716f6d04d6ba2c422c5d7f976
Marcel Göhn <marcelgoehn@googlemail.com>
2025-08-09 17:26:13 +0200

    Started to implement technologies section

--- src/app/portfolio-page/portfolio-page.component.ts
+++ src/app/portfolio-page/portfolio-page.component.ts
@@ -1,11 +1,12 @@
 import { Component, HostListener } from '@angular/core';
 import { HeroComponent } from "./hero/hero.component";
 import { AboutMeComponent } from "./about-me/about-me.component";
+import { TechnologiesComponent } from "./technologies/technologies.component";
 
 @Component({
   selector: 'app-portfolio-page',
   standalone: true,
-  imports: [HeroComponent, AboutMeComponent],
+  imports: [HeroComponent, AboutMeComponent, TechnologiesComponent],
   templateUrl: './portfolio-page.component.html',
   styleUrl: './portfolio-page.component.scss'
 })
