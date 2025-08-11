4458c2906e07a958e1f37f52405ef97c120be3d0
Marcel Göhn <marcelgoehn@googlemail.com>
2025-08-11 12:14:45 +0200

    Added the right side of the technologies section with the icons

--- src/app/portfolio-page/technologies/technologies.component.ts
+++ src/app/portfolio-page/technologies/technologies.component.ts
@@ -1,12 +1,69 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-technologies',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './technologies.component.html',
   styleUrl: './technologies.component.scss'
 })
 export class TechnologiesComponent {
-
+  skills = [
+    {
+      path: 'assets/img/technologies/html.svg',
+      alt: 'HTML Logo',
+      name: 'HTML'
+    },
+    {
+      path: 'assets/img/technologies/css.svg',
+      alt: 'CSS Logo',
+      name: 'CSS'
+    },
+    {
+      path: 'assets/img/technologies/javascript.svg',
+      alt: 'JavaScript Logo',
+      name: 'JavaScript'
+    },
+    {
+      path: 'assets/img/technologies/material-design.svg',
+      alt: 'Material Design Logo',
+      name: 'Material Design'
+    },
+    {
+      path: 'assets/img/technologies/typescript.svg',
+      alt: 'TypeScript Logo',
+      name: 'TypeScript'
+    },
+    {
+      path: 'assets/img/technologies/angular.svg',
+      alt: 'Angular Logo',
+      name: 'Angular'
+    },
+    {
+      path: 'assets/img/technologies/firebase.svg',
+      alt: 'Firebase Logo',
+      name: 'Firebase'
+    },
+    {
+      path: 'assets/img/technologies/git.svg',
+      alt: 'Git Logo',
+      name: 'Git'
+    },
+    {
+      path: 'assets/img/technologies/api.svg',
+      alt: 'REST-API Logo',
+      name: 'REST-API'
+    },
+    {
+      path: 'assets/img/technologies/scrum.svg',
+      alt: 'Scrum Logo',
+      name: 'Scrum'
+    },
+    {
+      path: 'assets/img/technologies/growth-mindset.svg',
+      alt: 'Growth Mindset Logo',
+      name: 'Growth mindset'
+    },
+  ];
 }
