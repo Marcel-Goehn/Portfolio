44104933f8356a5a2c70eac2d4604da8612357ed
Marcel Göhn <marcelgoehn@googlemail.com>
2025-09-06 14:23:15 +0200

    added carousel component

---
+++ src/app/portfolio-page/carousel/carousel.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CardComponent } from "../../shared/card/card.component";
+
+@Component({
+  selector: 'app-carousel',
+  standalone: true,
+  imports: [CardComponent],
+  templateUrl: './carousel.component.html',
+  styleUrl: './carousel.component.scss'
+})
+export class CarouselComponent {
+
+}
--- src/app/portfolio-page/portfolio-page.component.ts
+++ src/app/portfolio-page/portfolio-page.component.ts
@@ -3,11 +3,12 @@ import { HeroComponent } from "./hero/hero.component";
 import { AboutMeComponent } from "./about-me/about-me.component";
 import { TechnologiesComponent } from "./technologies/technologies.component";
 import { FeaturedProjectsComponent } from "./featured-projects/featured-projects.component";
+import { CarouselComponent } from "./carousel/carousel.component";
 
 @Component({
   selector: 'app-portfolio-page',
   standalone: true,
-  imports: [HeroComponent, AboutMeComponent, TechnologiesComponent, FeaturedProjectsComponent],
+  imports: [HeroComponent, AboutMeComponent, TechnologiesComponent, FeaturedProjectsComponent, CarouselComponent],
   templateUrl: './portfolio-page.component.html',
   styleUrl: './portfolio-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
