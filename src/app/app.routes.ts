df6b616b3fec24d15f750659b4e62ed3aa755988
Marcel Göhn <marcelgoehn@googlemail.com>
2025-09-11 20:25:01 +0200

    added legal notice markup and style

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { PortfolioPageComponent } from './portfolio-page/portfolio-page.component';
+import { LegalNoticeComponent } from './legal-notice/legal-notice.component';
 
 export const routes: Routes = [
-    {path: '', component: PortfolioPageComponent}
+    { path: '', component: PortfolioPageComponent },
+    { path: 'legal-notice', component: LegalNoticeComponent }
 ];
