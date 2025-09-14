7b3414f3c5a6acd912e9e88977559c06856a014d
Marcel Göhn <marcelgoehn@googlemail.com>
2025-09-14 12:04:32 +0200

    Added cursor background shadow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,27 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { CommonModule, NgStyle } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./shared/header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, NgStyle],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  host: {
+    '(mousemove)': 'calculateMouseShadow($event)' 
+  }
 })
 export class AppComponent {
-  
+  topPosition = signal<number | null>(null);
+  leftPosition = signal<number | null>(null);
+
+
+  calculateMouseShadow(e: MouseEvent) {
+    this.topPosition.set(e.clientY);
+    this.leftPosition.set(e.clientX);
+  }
+
 }
