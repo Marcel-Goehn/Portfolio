edb1955e53c0844970027b9ccb904c66c4a8e0e3
Marcel Göhn <marcelgoehn@googlemail.com>
2025-09-20 12:20:50 +0200

    added marquee to the header component

--- src/app/portfolio-page/hero/hero.component.ts
+++ src/app/portfolio-page/hero/hero.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, input, signal } from '@angular/core';
 import { ButtonComponent } from "../../shared/button/button.component";
+import { MarqueeComponent } from "./marquee/marquee.component";
 
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [CommonModule, ButtonComponent],
+  imports: [CommonModule, ButtonComponent, MarqueeComponent],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
