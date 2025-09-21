76c9d559fb74309f3120b3a0b6f7b5edc15d5569
Marcel Göhn <marcelgoehn@googlemail.com>
2025-09-21 13:10:19 +0200

    Removed swiper js and did build the carousel on my own the only thing left is to get the infinite scrolling going

--- src/app/portfolio-page/carousel/carousel.component.ts
+++ src/app/portfolio-page/carousel/carousel.component.ts
@@ -1,14 +1,80 @@
-import { ChangeDetectionStrategy, Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { AfterViewInit, ChangeDetectionStrategy, Component, ElementRef, HostListener, signal, viewChild } from '@angular/core';
 
 @Component({
   selector: 'app-carousel',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './carousel.component.html',
   styleUrl: './carousel.component.scss',
-  schemas: [CUSTOM_ELEMENTS_SCHEMA],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CarouselComponent {
+export class CarouselComponent implements AfterViewInit {
+  private carouselEl = viewChild.required<ElementRef<HTMLUListElement>>('carousel');
+  private firstCardEl = viewChild.required<ElementRef<HTMLElement>>('card');
+  firstCardWidth = signal<null | number>(null);
+  carouselChildrens = signal<Element[] | []>([]);
 
+  isDragging = signal(false);
+  startX = signal(0);
+  startScrollLeft = signal(0);
+
+  cardPerView = signal(0);
+
+  noTransition = signal(false);
+
+
+  ngAfterViewInit(): void {
+    this.firstCardWidth.set(this.firstCardEl().nativeElement.offsetWidth);
+    this.carouselChildrens.set(Array.from(this.carouselEl().nativeElement.children));
+    this.cardPerView.set(Math.round(this.carouselEl().nativeElement.offsetWidth / this.firstCardWidth()!));
+
+    this.carouselChildrens().slice(-this.cardPerView()).reverse().forEach(card => {
+      this.carouselEl().nativeElement.insertAdjacentHTML('afterbegin', card.outerHTML);
+    });
+
+    this.carouselChildrens().slice(0, this.cardPerView()).forEach(card => {
+      this.carouselEl().nativeElement.insertAdjacentHTML('beforeend', card.outerHTML);
+    });
+  }
+
+
+  buttonScroll(scrollSide: string) {
+    this.carouselEl().nativeElement.scrollLeft += scrollSide === 'left' ? -this.firstCardWidth()! : this.firstCardWidth()!;
+  }
+
+
+  dragStart(e: MouseEvent) {
+    this.isDragging.set(true);
+    this.startX.set(e.pageX);
+    this.startScrollLeft.set(this.carouselEl().nativeElement.scrollLeft);
+    console.log(this.startScrollLeft());
+  }
+
+
+  @HostListener('document:mouseup')
+  dragEnd() {
+    this.isDragging.set(false);
+  }
+
+
+  dragging(e: MouseEvent) {
+    if (!this.isDragging()) return;
+
+    this.carouselEl().nativeElement.scrollLeft = this.startScrollLeft() - (e.pageX - this.startX());
+  }
+
+
+  infiniteScroll() {
+    if (this.carouselEl().nativeElement.scrollLeft === 0) {
+      this.noTransition.set(true);
+      this.carouselEl().nativeElement.scrollLeft = this.carouselEl().nativeElement.scrollWidth - (2 * this.carouselEl().nativeElement.offsetWidth);
+      this.noTransition.set(false);
+    }
+    else if (Math.ceil(this.carouselEl().nativeElement.scrollLeft) === this.carouselEl().nativeElement.scrollWidth - this.carouselEl().nativeElement.offsetWidth) {
+      this.noTransition.set(true);
+      this.carouselEl().nativeElement.scrollLeft = this.carouselEl().nativeElement.offsetWidth;
+      this.noTransition.set(false);
+    }
+  }
 }
