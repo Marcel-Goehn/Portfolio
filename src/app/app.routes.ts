43067cdfecb2a55f725a1b173b93e843486102e9
Marcel Göhn <marcelgoehn@googlemail.com>
2025-09-28 14:01:02 +0200

    Added route lazy loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,20 @@
 import { Routes } from '@angular/router';
 import { PortfolioPageComponent } from './portfolio-page/portfolio-page.component';
-import { LegalNoticeComponent } from './legal-notice/legal-notice.component';
-import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
 
 export const routes: Routes = [
-    { path: '', component: PortfolioPageComponent, title: 'Marcel Göhn | Portfolio' },
-    { path: 'legal-notice', component: LegalNoticeComponent, title: 'Marcel Göhn | Legal Notice'},
-    { path: 'privacy-policy', component: PrivacyPolicyComponent, title: 'Marcel Göhn | Privacy Policy' }
+    {
+        path: '',
+        component: PortfolioPageComponent,
+        title: $localize`Marcel Göhn | Portfolio`
+    },
+    {
+        path: 'legal-notice',
+        loadComponent: () => import('./legal-notice/legal-notice.component').then(mod => mod.LegalNoticeComponent),
+        title: $localize`Marcel Göhn | Legal Notice`
+    },
+    {
+        path: 'privacy-policy',
+        loadComponent: () => import('./privacy-policy/privacy-policy.component').then(mod => mod.PrivacyPolicyComponent),
+        title: $localize`Marcel Göhn | Privacy Policy`
+    }
 ];
